f26770f7db38d08f7c45796c780e55f68c550e8a
junkshark8 <152639308+junkshark8@users.noreply.github.com>
2024-04-18 08:39:37 -0500

    move footer style to styles.css

--- components/footer.js
+++ components/footer.js
@@ -12,58 +12,6 @@ class Footer extends HTMLElement {
     // fires when component is render into DOM
     connectedCallback() {
         this.innerHTML = `
-        <style>
-            footer {
-                width: 100%;
-                padding: 1rem 1.5rem;
-                background-color: var(--yellow);
-                color: var(--dark);
-                font-size: 14px;
-                display: flex;
-                position: relative;
-                z-index: 2;
-                left: 0;
-                bottom: 0;
-                justify-content: space-between;
-                align-items: center;
-                border-top: 2px solid var(--purple);
-
-                .contact {
-                    display: flex;
-                    justify-content: space-between;
-                    align-items: center;
-                }
-
-                a {
-                    display: flex;
-                    align-items: center;
-                    margin-inline: 0.5rem;
-                    border: 2px solid;
-                    border-color: var(--purple); 
-                    border-radius: 5px;
-                    text-decoration: none;
-                    color: var(--dark);
-                    font-size: 1.5rem;
-                    font-weight: 300;
-                    height: 2rem;
-                    padding: 0.25rem;
-                  }
-
-                .edge {
-                    margin-right: 0;
-                }
-                
-                a:hover, a:focus {
-                    border-color: var(--dark);
-                    border-style: dotted;
-                  }
-
-                i {
-                    padding: 0;
-                }
-            }
-        </style>
-            
         <footer>
             <p>© ` + copyrightYear + ` Kate Doolittle. Hand coded by me, Kate Doolittle :P</p>
             <div class='contact'>
